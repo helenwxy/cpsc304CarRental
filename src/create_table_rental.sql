85c3184fca97eba3938929639b41afdefc4cd62b
helenwxy <40188330+helenwxy@users.noreply.github.com>
2019-11-17 22:44:28 -0800

    initial commit

---
+++ src/create_table_rental.sql
@@ -0,0 +1,73 @@
+create table customer (
+    dlicense varchar(20) not null PRIMARY KEY,
+    name varchar(20) not null,
+    address varchar(20) not null,
+    phone integer
+);
+
+create table vehicleType (
+    vtname varchar(20) not null PRIMARY KEY,
+    feature varchar(20) not null,
+    wrate float not null,
+    drate float not null,
+    hrate float not null,
+    wirate float not null,
+    dirate float not null,
+    hirate float not null,
+    krate float not null
+);
+
+create table vehicle (
+    vlicense varchar(20) not null PRIMARY KEY,
+    vid integer not null PRIMARY KEY,
+    make varchar(20) not null,
+    model varchar(20) not null,
+    year integer not null,
+    odometer integer not null,
+    status varchar(20) not null,
+    vtname not null,
+    location varchar(20) not null,
+    city varchar(20) not null,
+    foreign key (vtname) references vehicleType,
+    CHECK (status IN ('available', 'rented', 'maintenance'))
+);
+
+create table reservation (
+    confNo integer not null PRIMARY KEY,
+    vtname varchar(20) not null,
+    dlicense varchar(20) not null,
+    fromDate date not null,
+    toDate date not null,
+    fromTime date not null,
+    toTime date not null,
+    foreign key (vtname) references vehicleType,
+    foreign key (dlicense) references customer
+);
+
+create table rental (
+    rid integer not null PRIMARY KEY,
+    vlicense integer not null,
+    dlicense varchar(20) not null,
+    fromDate date not null,
+    toDate date not null,
+    fromTime date not null,
+    toTime date not null,
+    odometer integer not null,
+    cardName varchar(20) not null,
+    cardNo integer not null,
+    expDate date not null,
+    confNo integer,
+    foreign key (vlicense) references vehicle,
+    foreign key (dlicense) references customer,
+    foreign key (confNo) references reservation
+);
+
+create table return (
+    rid integer not null PRIMARY KEY,
+    rdate date not null,
+    rtime date not null,
+    odometer integer not null,
+    fulltank number not null,
+    value float not null,
+    foreign key (rid) references rental
+);
---
+++ src/insert_rows_rental.sql
@@ -0,0 +1,13 @@
+insert into customer values ('GA123456', 'Bob Smith', '111 E. 11 St., Vancouver', 6043121234);
+insert into customer values ('GC222222', 'Candace Walters', '222 E. 22 St., Burnaby', 6049875432);
+insert into customer values ('BA112345', 'Kara Rops', '333 W. 33 Ave., Richmond', 7781234444);
+insert into customer values ('AC123145', 'Alex Mark', '444 E. 4 Ave., Vancouver', 6041111111);
+insert into customer values ('EE123111', 'John Smith', '414 E. 26 Ave., Vancouver', 6049898989);
+
+insert into vehicleType values ('Economy', 'Auto lock', 300, 50, 10, 200, 40, 5, 2);
+insert into vehicleType values ('Compact', 'AC', 200, 30, 5, 200, 40, 5, 2);
+insert into vehicleType values ('Mid-size', 'Heated seat', 350, 55, 12, 200, 40, 5, 2);
+insert into vehicleType values ('Standard', 'Heated seat', 400, 60, 15, 200, 40, 5, 2);
+insert into vehicleType values ('Full-size', 'Leather seat', 450, 65, 20, 300, 50, 10, 2.5);
+insert into vehicleType values ('SUV', 'Big trunk size', 500, 70, 25, 350, 60, 15, 3);
+insert into vehicleType values ('Truck', 'Cargo space', 550, 80, 30, 400, 70, 20, 3.5);
